0769a82911e13dc7952c2ccb4615da08dbc6827f
Nan Monnand Deng <monnand@gmail.com>
2014-03-28 17:43:55 -0400

    FC.

--- tyrion-worker/main.go
+++ tyrion-worker/main.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"net/http"
-	"net/url"
+	"os"
 )
 
 var argDaemon = flag.Bool("d", false, "set this parameter to run it as a server")
-var argTaskFile = flag.String("json", "./task.json", "the file container a task in json format")
+var argBind = flag.String("bind", "0.0.0.0:9891", "bind address for the HTTP server. Only work if -d is specified")
+var argJsonFile = flag.String("json", "./task.json", "the file container a task in json format")
 var argNrWorkers = flag.Int("n", 10, "number of concurrent workers")
-
-type HttpResponseReader struct {
-}
-
-func (self *HttpResponseReader) ReadResponse(tag, url, method, content string, params url.Values, headers http.Header) (status int, body io.ReadCloser, err error) {
-	req, err := http.NewRequest(method, url, bytes.NewBufferString(content))
-	if err != nil {
-		return
-	}
-	req.Header = headers
-	req.Form = params
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return
-	}
-	status = resp.StatusCode
-	body = resp.Body
-	return
-}
 
 func main() {
 	flag.Parse()
-	StartWorkers(*argNrWorkers)
+	N := *argNrWorkers
+	if N <= 0 {
+		N = 1
+	}
+	StartWorkers(N)
+	rr := &HttpResponseReader{}
+	server := NewTaskServer(rr)
+	var err error
+	if *argDaemon {
+		err = http.ListenAndServe(*argBind, server)
+	} else if len(*argJsonFile) > 0 {
+		var f io.ReadCloser
+		f, err = os.Open(*argJsonFile)
+		if err == nil {
+			defer f.Close()
+			server.ServeJson(os.Stdout, f)
+		}
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
 }
