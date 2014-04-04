3797f0d5f139e585c55537e6132552d6a5e3b566
Nan Monnand Deng <monnand@gmail.com>
2014-04-04 17:23:09 -0400

    httprr

---
+++ tyrion-worker/httprr.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func init() {
+	RegisterPlugin(&HttpResponseReaderFactory{})
+}
+
+type HttpResponseReaderFactory struct {
+}
+
+func (self *HttpResponseReaderFactory) String() string {
+	return "http"
+}
+
+// This plugin does not accept rest in the chain.
+func (self *HttpResponseReaderFactory) NewPlugin(params map[string]string, rest ResponseReader) (rr ResponseReader, err error) {
+	rr = &HttpResponseReader{}
+	return
+}
+
+type HttpResponseReader struct {
+}
+
+func (self *HttpResponseReader) ReadResponse(req *Request, env *Env) (resp *Response, updates *Env, err error) {
+	var httpResp *http.Response
+	if req.Params != nil {
+		httpResp, err = http.PostForm(req.URL, req.Params)
+	} else {
+		var r *http.Request
+		r, err = http.NewRequest(req.Method, req.URL, bytes.NewBufferString(req.Content))
+		if err != nil {
+			return
+		}
+		r.Header = req.Headers
+		client := &http.Client{}
+		httpResp, err = client.Do(r)
+	}
+	if err != nil {
+		return
+	}
+	resp = new(Response)
+	resp.Status = httpResp.StatusCode
+	// resp.Body = httpResp.Body
+	defer httpResp.Body.Close()
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		return
+	}
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	return
+}
---
+++ tyrion-worker/httprr_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func testHttpResponseReader(t *testing.T, method, content string, params url.Values, headers http.Header) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+		if r.Method != method {
+			t.Errorf("Method should be %v; Received %v", method, r.Method)
+		}
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+		c := string(b)
+		if c != content {
+			t.Errorf("content should be %v; Received %v", content, c)
+		}
+
+		if len(params) > 0 {
+			for k, _ := range params {
+				if params.Get(k) != r.PostForm.Get(k) {
+					t.Errorf("Parameter %v should be %v; received %v", k, params.Get(k), r.PostForm.Get(k))
+				}
+			}
+		}
+		if len(headers) > 0 {
+			for k, _ := range headers {
+				if headers.Get(k) != r.Header.Get(k) {
+					t.Errorf("Header %v should be %v; received %v", k, headers.Get(k), r.Header.Get(k))
+				}
+			}
+		}
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	rr := &HttpResponseReader{}
+	req := &Request{
+		Tag:     "test",
+		URL:     ts.URL,
+		Method:  method,
+		Content: content,
+		Params:  params,
+		Headers: headers,
+	}
+	resp, u, err := rr.ReadResponse(req, nil)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if resp == nil {
+		t.Errorf("Http response is nil")
+	} else if resp.Body == nil {
+		t.Errorf("Http response body is nil")
+	} else {
+		defer resp.Body.Close()
+	}
+	if !u.IsEmpty() {
+		t.Errorf("Updated env should be empty: %v", u)
+	}
+	if resp.Status != 200 {
+		t.Errorf("Status: %v", resp.Status)
+	}
+}
+
+func TestHttpResponseReader(t *testing.T) {
+	testHttpResponseReader(t, "GET", "", nil, nil)
+	testHttpResponseReader(t, "POST", "", nil, nil)
+	testHttpResponseReader(t, "GET", "content", nil, nil)
+	params := url.Values{}
+	params.Set("hello", "world")
+	params.Set("hello2", "world2")
+	testHttpResponseReader(t, "POST", "", params, nil)
+	headers := http.Header{}
+	headers.Set("hello", "world")
+	headers.Set("hello2", "world2")
+	testHttpResponseReader(t, "GET", "", nil, headers)
+	testHttpResponseReader(t, "GET", "something", nil, headers)
+	testHttpResponseReader(t, "POST", "hello", nil, headers)
+}
